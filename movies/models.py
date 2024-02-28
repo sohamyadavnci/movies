15997f3d4cd10210915459284f062e44023c49fd
EC2 Default User <ec2-user@ip-172-31-27-96.eu-west-1.compute.internal>
2024-02-28 12:11:21 +0000

    Added allowed host for eb

--- movies/models.py
+++ movies/models.py
@@ -4,15 +4,11 @@ from django.db import models
 from django.shortcuts import loader
 
 class Movie(models.Model):
-    
-    
- title = models.CharField(max_length=200)
- director = models.CharField(max_length=30)
- release_date = models.DateTimeField('release date')
- genre = models.CharField(max_length=200)
- duration = models.FloatField()
+    title = models.CharField(max_length=200)
+    director = models.CharField(max_length=30)
+    release_date = models.DateTimeField('release date')
+    genre = models.CharField(max_length=200)
+    duration = models.FloatField()
 
-
-    
- def __str__(self):
-    return self.title + " - " + self.director
+    def __str__(self):
+        return self.title + " - " + self.director
