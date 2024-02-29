a6b4b3fd279a705cf37f2f6408cffd1c8624ea6a
EC2 Default User <ec2-user@ip-172-31-27-96.eu-west-1.compute.internal>
2024-02-29 11:54:03 +0000

    Comment added

--- demoproj/urls.py
+++ demoproj/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -26,3 +27,5 @@ urlpatterns = [
     path('signout/', auth_views.LogoutView.as_view(
         template_name='users/signout.html'), name='sign_out'),
 ]
+
+#To be added
