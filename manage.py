986f88c965bdd1965a1b102cda51d8951dab2280
EC2 Default User <ec2-user@ip-172-31-27-96.eu-west-1.compute.internal>
2024-03-12 12:57:18 +0000

    pyhton version changed in buildspec

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python
+"""Management script for handling various tasks in the project."""
 import os
 import sys
+
+
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demoproj.settings')
